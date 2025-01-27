80a64c4bccff445e9e3d6d6d16b4bc4d155caa78
Sephelim <1001891@warwicksd.org>
2025-01-27 11:15:53 -0500

    updated for new namespace structure

--- Khaal_Scripts/Entry.js
+++ Khaal_Scripts/Entry.js
@@ -10,10 +10,15 @@ export async function Main(gl) {
   await Selenium.Input.Keyboard.LoadMap('default');
   Selenium.Input.Keyboard.Enable();
 
-  await Selenium.Graphics.Shaders.Register('basic');
-  Selenium.Graphics.Shaders.Use('basic');
+  await Selenium.Assets.Shaders.Register('basic');
+  Selenium.Assets.Shaders.Use('basic');
 
-  const cube = new Selenium.Graphics.Basic.Cube({x: 500, y: 100, z: -25});
+  //! need global texture table to save memory!
+
+  const cube = new Selenium.Graphics.Basic.Cube(
+      {x: 500, y: 100, z: -25}, undefined, undefined, 'placeholder.png');
+  const pyramid = new Selenium.Graphics.Basic.Pyramid(
+      {x: 500, y: 200, z: -25}, undefined, undefined, 'placeholder.png');
 
   //! we need an arrow basic shape class
   const lines_buffer = Selenium.Graphics.Buffers.VO(new Float32Array([
@@ -32,25 +37,26 @@ export async function Main(gl) {
   gl.enableVertexAttribArray(1);
 
   Selenium.Input.Camera.SetView('basic');
-  Selenium.Graphics.Shaders.SetUniform(
+  Selenium.Assets.Shaders.SetUniform(
       'basic', 'v3_light_position', GLMatrix.Vec3.fromValues(50, 500, 50));
 
   Selenium.RegisterRenderer(() => {
     Selenium.Graphics.ClearScreen(0.0, 0.0, 0.0);
-    Selenium.Graphics.Shaders.SetUniform(
+    Selenium.Assets.Shaders.SetUniform(
         'basic', 'm4_projection_matrix', Selenium.Graphics.Projection);
-    Selenium.Graphics.Shaders.SetUniform('basic', 'b_show_normals', false);
+    Selenium.Assets.Shaders.SetUniform('basic', 'b_show_normals', false);
 
-    cube.Render('basic');
+    cube.Render('basic');  //? why gone when second shape
+    pyramid.Render('basic');
 
     if (Selenium.Data.Mode == 0) {
       const model_matrix = GLMatrix.Mat4.create();
 
       GLMatrix.Mat4.translate(
           model_matrix, model_matrix, GLMatrix.Vec3.fromValues(50, 0, -25));
-      Selenium.Graphics.Shaders.SetUniform(
+      Selenium.Assets.Shaders.SetUniform(
           'basic', 'm4_model_matrix', model_matrix);
-      Selenium.Graphics.Shaders.SetUniform(
+      Selenium.Assets.Shaders.SetUniform(
           'basic', 'v3_model_color', GLMatrix.Vec3.fromValues(1.0, 0.25, 0.5));
 
       gl.bindVertexArray(lines_buffer);
